d6afa382e854311686f50b3ad1d7e515c19035e8
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-02-04 05:54:20 +0530

    Add files via upload

---
+++ IfTest.java
@@ -0,0 +1,16 @@
+
+public class IfTest {
+    public static void main(String[] args) {
+        int age=28;
+        int qualifiedAge=18;
+        System.out.println("Begin with main");
+        //operand1 >= operand2
+        if(age>=qualifiedAge)
+            System.out.println("You can vote");
+        else
+            System.out.println("You cannot vote");
+
+        System.out.println("End of main");
+    }
+}
+
