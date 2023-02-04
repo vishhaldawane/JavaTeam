a7f60f90cd573d41eff744bbc51b06e71a0682ae
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-02-04 06:31:28 +0530

    If else if else if

--- IfTest.java
+++ IfTest.java
@@ -1,14 +1,32 @@
 
 public class IfTest {
     public static void main(String[] args) {
-        int age=28;
+        int age=8;
         int qualifiedAge=18;
+        int maxHumanAge=120;
         System.out.println("Begin with main");
         //operand1 >= operand2
-        if(age>=qualifiedAge)
-            System.out.println("You can vote");
+
+        if(age < 0)
+            System.out.println("Invalid human age");
         else
-            System.out.println("You cannot vote");
+        {
+            if (age > maxHumanAge)
+                System.out.println("Usually human age should not exceed 120");
+            else
+            {
+                if (age >= qualifiedAge)
+                {
+                    System.out.println("You can vote");
+                    System.out.println("You should have voting Id");
+                } else
+                {
+                    System.out.println("You can not vote");
+                    System.out.println("You should not have voting Id");
+                }
+            }
+        }
+        System.out.println("some thing here...");
 
         System.out.println("End of main");
     }
